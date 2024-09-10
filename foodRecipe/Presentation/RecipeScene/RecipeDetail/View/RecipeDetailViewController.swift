1b1d90ff952e45ea6d2d2f2d67bf70afd38c34bd
marha-hwang <hwangharam@naver.com>
2024-09-10 21:11:05 +0900

    feat. 레시피 상세화면 구현중

--- foodRecipe/Presentation/RecipeScene/RecipeDetail/View/RecipeDetailViewController.swift
+++ foodRecipe/Presentation/RecipeScene/RecipeDetail/View/RecipeDetailViewController.swift
@@ -26,50 +26,76 @@ class RecipeDetailViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
-        print(viewModel.recipe.recipe_name)
         viewModel.viewDidLoad()
+        setupViews()
+        prepareSubViewController()
+        setupBehaviours()
+        bind(to: viewModel)
+    }
+    
+    private func bind(to viewModel: RecipeDetailViewModel){
         
+    }
+    
+    private func prepareSubViewController(){
+        
+    }
+    
+    private func setupBehaviours(){
         addBehaviors([DefaultNavigationBarBehavior(), HideTabBarBehavior()])
-        
-        name.translatesAutoresizingMaskIntoConstraints = false
-        status.translatesAutoresizingMaskIntoConstraints = false
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        name.text = viewModel.recipe.recipe_name
-        
-        viewModel.favoriteStatus.observe(on: self){[weak self] _ in
-            DispatchQueue.main.async{
-                self?.status.text = self?.viewModel.favoriteStatus.value == true ? "true":"false"
-            }
-        }
-        
-        button.backgroundColor = .black
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(searchButtonEvent)) 
-        button.isUserInteractionEnabled = true
-        button.addGestureRecognizer(gestureRecognizer)
-        
-        view.addSubview(name)
-        view.addSubview(status)
-        view.addSubview(button)
-        
-        name.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
-        name.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        name.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        status.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 30).isActive = true
-        status.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        status.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        button.topAnchor.constraint(equalTo: status.bottomAnchor, constant: 30).isActive = true
-        button.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        
     }
     
-    @objc private func searchButtonEvent(sender: UITapGestureRecognizer){
-        viewModel.didTouchFavorite()
+    private func setupViews(){
+        
     }
-    
 }
+
+//var name = UILabel()
+//var status = UILabel()
+//var button = UIView()
+//
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//    viewModel.viewDidLoad()
+//    addBehaviors([DefaultNavigationBarBehavior(), HideTabBarBehavior()])
+//    
+//    name.translatesAutoresizingMaskIntoConstraints = false
+//    status.translatesAutoresizingMaskIntoConstraints = false
+//    button.translatesAutoresizingMaskIntoConstraints = false
+//    
+//    name.text = viewModel.recipe.recipe_name
+//    
+//    viewModel.favoriteStatus.observe(on: self){[weak self] _ in
+//        DispatchQueue.main.async{
+//            self?.status.text = self?.viewModel.favoriteStatus.value == true ? "true":"false"
+//        }
+//    }
+//    
+//    button.backgroundColor = .black
+//    let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(searchButtonEvent))
+//    button.isUserInteractionEnabled = true
+//    button.addGestureRecognizer(gestureRecognizer)
+//    
+//    view.addSubview(name)
+//    view.addSubview(status)
+//    view.addSubview(button)
+//    
+//    name.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+//    name.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+//    name.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//    
+//    status.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 30).isActive = true
+//    status.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+//    status.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//    
+//    button.topAnchor.constraint(equalTo: status.bottomAnchor, constant: 30).isActive = true
+//    button.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+//    button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+//    button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//    
+//}
+//
+//@objc private func searchButtonEvent(sender: UITapGestureRecognizer){
+//    viewModel.didTouchFavorite()
+//}
+//
